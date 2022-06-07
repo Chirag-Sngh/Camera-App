b512427a9a2029150fe1ebe8a2d76f546489cce1
Chirag <chiragsingh21001@gmail.com>
2022-06-08 03:19:22 +0530

    Camera

---
+++ app/src/main/java/com/example/chira/cameraappprac/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.chira.cameraappprac;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView iv;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv=(ImageView)findViewById(R.id.imageView);
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,69);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==69)
+        {
+            Bitmap bm=(Bitmap)data.getExtras().get("data");
+            iv.setImageBitmap(bm);
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "camera not working", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
